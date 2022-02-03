1dd4c1ee924ac0caf8c4234bb8e0f1fea73442f7
PaulaEPR <paulaperera@gmail.com>
2022-02-03 11:29:49 +0100

    static server

--- src/index.js
+++ src/index.js
@@ -1,22 +1,39 @@
 const express = require('express');
 const cors = require('cors');
 const movies = require('./data/movies.json');
+const users = require('./data/users.json');
 // create and config server
-const server = express();
-server.use(cors());
-server.use(express.json());
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 // init express aplication
 const serverPort = 4000;
-server.listen(serverPort, () => {
+app.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-server.get("/movies", (req, res)=>{
+app.get('/movies', (req, res) => {
+  console.log(movies);
   const response = {
     success: true,
-  movies: movies(),
-} 
-res.json(response);
-})
+    movies: movies,
+  };
+  res.json(response);
+});
 
+app.post('/login', (req, res) => {
+  console.log(users);
+  const response = {
+    success: true,
+    users: users,
+  };
+  res.json(response);
+});
+
+//static server
+const staticServerPathWeb = './public-react';
+app.use(express.static(staticServerPathWeb));
+
+const staticServerPathImage = './src/public-movies-images';
+app.use(express.static(staticServerPathImage));
