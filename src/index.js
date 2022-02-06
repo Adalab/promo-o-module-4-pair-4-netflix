927c9dca339584fe7cef88f2bedc5a78037d99e8
PaulaEPR <paulaperera@gmail.com>
2022-02-06 21:40:38 +0100

    finish render movie

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const movies = require('./data/movies.json');
 const users = require('./data/users.json');
+
 // create and config server
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.set("view engine","ejs")
+app.set('view engine', 'ejs');
 
 // init express aplication
 const serverPort = 4000;
@@ -14,6 +15,7 @@ app.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
+//endpoints
 app.get('/movies', (req, res) => {
   console.log(movies);
   const response = {
@@ -32,21 +34,21 @@ app.post('/login', (req, res) => {
   res.json(response);
 });
 
-
-//url params
-
-app.get("/movie/:movieId",(req, res) => {
+// endpoints with url params
+app.get('/movie/:movieId', (req, res) => {
   const requestMovieId = req.params.movieId;
   console.log(requestMovieId);
-  const movieData = movies.find(movie => movie.id === requestMovieId);
+  const movieData = movies.find((movie) => movie.id === requestMovieId);
   console.log(movieData);
-  res.render("movie",movieData);
-}) 
+  res.render('movie', movieData);
+});
 
-//static server
+//static servers
 const staticServerPathWeb = './public-react';
 app.use(express.static(staticServerPathWeb));
 
 const staticServerPathImage = './src/public-movies-images';
 app.use(express.static(staticServerPathImage));
 
+const staticServerPathCSS = './src/styles';
+app.use(express.static(staticServerPathCSS));
